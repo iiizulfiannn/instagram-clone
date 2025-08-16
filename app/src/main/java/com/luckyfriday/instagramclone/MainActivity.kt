f2ddfa234dc789823da99723be5cb9cbcbb122a5
zul <iiizulfiannn@gmail.com>
2025-08-16 18:56:10 +0700

    feat: add header home fragment

--- app/src/main/java/com/luckyfriday/instagramclone/MainActivity.kt
+++ app/src/main/java/com/luckyfriday/instagramclone/MainActivity.kt
@@ -5,14 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.luckyfriday.instagramclone.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setFragmentContainer(HomeFragment.newInstance())
         findViewById<BottomNavigationView>(R.id.nav_view)?.apply {
             itemIconTintList = null
         }
+    }
+
+    private fun setFragmentContainer(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
