a8f4c4562a273d685c7036fd5751e1fd4b88e382
zul <iiizulfiannn@gmail.com>
2025-08-20 17:02:35 +0700

    feat: add adapter home story

---
+++ app/src/main/java/com/luckyfriday/instagramclone/adapters/HomeStoryAdapter.kt
@@ -0,0 +1,39 @@
+package com.luckyfriday.instagramclone.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.luckyfriday.instagramclone.R
+import com.luckyfriday.instagramclone.models.HomeStoryModel
+
+class HomeStoryAdapter(
+    private val list: List<HomeStoryModel>
+) : RecyclerView.Adapter<HomeStoryAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.cell_home_story, parent, false
+        )
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = list[position]
+        holder.profileImage.setImageResource(item.image)
+        holder.nameTextView.text = item.name
+        if (position == 0) holder.addStoryIcon.visibility = View.VISIBLE
+        else holder.addStoryIcon.visibility = View.GONE
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val profileImage: ImageView = itemView.findViewById(R.id.iv_home_story)
+        val addStoryIcon: ImageView = itemView.findViewById(R.id.iv_add_story)
+        val nameTextView: TextView = itemView.findViewById(R.id.tv_home_story_name)
+    }
+
+}
---
+++ app/src/main/java/com/luckyfriday/instagramclone/models/HomeStoryModel.kt
@@ -0,0 +1,7 @@
+package com.luckyfriday.instagramclone.models
+
+data class HomeStoryModel(
+    val image: Int,
+    val name: String,
+    val isLive: Boolean
+)
