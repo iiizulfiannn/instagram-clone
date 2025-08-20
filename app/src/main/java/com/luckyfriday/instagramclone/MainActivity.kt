29ff5966c714b16a7416a28f2063396a7a52bb0a
zul <iiizulfiannn@gmail.com>
2025-08-20 12:00:28 +0700

    feat: implement navigation bottom tab

--- app/src/main/java/com/luckyfriday/instagramclone/MainActivity.kt
+++ app/src/main/java/com/luckyfriday/instagramclone/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.luckyfriday.instagramclone.fragments.HomeFragment
+import com.luckyfriday.instagramclone.fragments.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,24 @@ class MainActivity : AppCompatActivity() {
         setFragmentContainer(HomeFragment.newInstance())
         findViewById<BottomNavigationView>(R.id.nav_view)?.apply {
             itemIconTintList = null
+            setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.navigation_home -> {
+                        setFragmentContainer(HomeFragment.newInstance())
+                        true
+                    }
+
+                    R.id.navigation_explore -> true
+                    R.id.navigation_post -> true
+                    R.id.navigation_reels -> true
+                    R.id.navigation_profile -> {
+                        setFragmentContainer(ProfileFragment.newInstance())
+                        true
+                    }
+
+                    else -> false
+                }
+            }
         }
     }
 
