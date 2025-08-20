6310818efcec6349abb0153d32cb015eeabac706
zul <iiizulfiannn@gmail.com>
2025-08-20 20:41:42 +0700

    feat: add populate local data home story and home post

--- app/src/main/java/com/luckyfriday/instagramclone/fragments/HomeFragment.kt
+++ app/src/main/java/com/luckyfriday/instagramclone/fragments/HomeFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.luckyfriday.instagramclone.R
+import com.luckyfriday.instagramclone.adapters.HomePostAdapter
+import com.luckyfriday.instagramclone.adapters.HomeStoryAdapter
+import com.luckyfriday.instagramclone.models.HomePostModel
+import com.luckyfriday.instagramclone.models.HomeStoryModel
 
 /**
  * A simple [Fragment] subclass.
@@ -20,6 +26,58 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val storyRecyclerView = view.findViewById<RecyclerView>(R.id.rv_home_story)
+        storyRecyclerView.layoutManager =
+            LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        val storyAdapter = HomeStoryAdapter(getHomeStoryList())
+        storyRecyclerView.adapter = storyAdapter
+
+        val postRecyclerView = view.findViewById<RecyclerView>(R.id.rv_home_post)
+        postRecyclerView.layoutManager =
+            LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+        val postAdapter = HomePostAdapter(getHomePostList())
+        postRecyclerView.adapter = postAdapter
+    }
+
+    private fun getHomeStoryList(): List<HomeStoryModel> {
+        return listOf(
+            HomeStoryModel(R.drawable.ic_avatar_big, "Codefive", false),
+            HomeStoryModel(R.drawable.img_profile_picture_1, "Arthur", true),
+            HomeStoryModel(R.drawable.img_profile_picture_2, "Arneo", false),
+            HomeStoryModel(R.drawable.img_profile_picture_3, "Nicolas", false),
+            HomeStoryModel(R.drawable.img_profile_picture_4, "barbie_girlzzz", false),
+            HomeStoryModel(R.drawable.img_profile_picture_1, "Budi", true),
+            HomeStoryModel(R.drawable.img_profile_picture_2, "Jojon", false),
+            HomeStoryModel(R.drawable.img_profile_picture_3, "Dyren", false),
+            HomeStoryModel(R.drawable.img_profile_picture_4, "Nunung", false)
+        )
+    }
+
+    private fun getHomePostList(): List<HomePostModel> {
+        return listOf(
+            HomePostModel(
+                R.drawable.img_profile_picture_2,
+                R.drawable.img_content_1,
+                "Areno Paris",
+                "Arneoo",
+                "Gabdu et d’autres personnes",
+                "ArthurHazan Lorem Ipsum #Proud",
+                35
+            ),
+            HomePostModel(
+                R.drawable.img_profile_picture_2,
+                R.drawable.img_content_1,
+                "Nicolas",
+                "Germany",
+                "Gabdu et d’autres personnes",
+                "Mungkin saja",
+                108
+            )
+        )
     }
 
     companion object {
