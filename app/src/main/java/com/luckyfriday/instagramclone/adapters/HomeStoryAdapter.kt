19d34fc13ba077e520c36694089f5cec1ac9c957
zul <iiizulfiannn@gmail.com>
2025-08-20 20:17:28 +0700

    feat: add multiple content type between general story and live story

--- app/src/main/java/com/luckyfriday/instagramclone/adapters/HomeStoryAdapter.kt
+++ app/src/main/java/com/luckyfriday/instagramclone/adapters/HomeStoryAdapter.kt
@@ -11,21 +11,49 @@ import com.luckyfriday.instagramclone.models.HomeStoryModel
 
 class HomeStoryAdapter(
     private val list: List<HomeStoryModel>
-) : RecyclerView.Adapter<HomeStoryAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.cell_home_story, parent, false
-        )
-        return ViewHolder(view)
+    override fun getItemViewType(position: Int): Int {
+        return if (list[position].isLive) 0 else 1
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            0 -> {
+                val view = LayoutInflater.from(parent.context).inflate(
+                    R.layout.cell_home_live, parent, false
+                )
+                LiveViewHolder(view)
+            }
+
+            else -> {
+                val view = LayoutInflater.from(parent.context).inflate(
+                    R.layout.cell_home_story, parent, false
+                )
+                ViewHolder(view)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val item = list[position]
-        holder.profileImage.setImageResource(item.image)
-        holder.nameTextView.text = item.name
-        if (position == 0) holder.addStoryIcon.visibility = View.VISIBLE
-        else holder.addStoryIcon.visibility = View.GONE
+
+        when (holder.itemViewType) {
+            0 -> {
+                (holder as LiveViewHolder).apply {
+                    profileImage.setImageResource(item.image)
+                    nameTextView.text = item.name
+                }
+            }
+            else -> {
+                holder as ViewHolder
+                holder.profileImage.setImageResource(item.image)
+                holder.nameTextView.text = item.name
+                if (position == 0) holder.addStoryIcon.visibility = View.VISIBLE
+                else holder.addStoryIcon.visibility = View.GONE
+            }
+        }
+
     }
 
     override fun getItemCount(): Int = list.size
@@ -34,5 +62,10 @@ class HomeStoryAdapter(
         val profileImage: ImageView = itemView.findViewById(R.id.iv_home_story)
         val addStoryIcon: ImageView = itemView.findViewById(R.id.iv_add_story)
         val nameTextView: TextView = itemView.findViewById(R.id.tv_home_story_name)
+    }
+
+    class LiveViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val profileImage: ImageView = itemView.findViewById(R.id.iv_home_live)
+        val nameTextView: TextView = itemView.findViewById(R.id.tv_home_live_name)
     }
 }
